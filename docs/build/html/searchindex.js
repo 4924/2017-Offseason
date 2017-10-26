4b13e0c1f41579a4e34137a0165e44a882b9bb0f
Elias Basler <e.e.basler@gmail.com>
2017-10-26 13:54:09 -0400

    Add docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to 2017-Offseason\u2019s documentation!"],titleterms:{document:0,indic:0,offseason:0,tabl:0,welcom:0}})
