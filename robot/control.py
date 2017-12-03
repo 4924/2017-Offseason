831ff30c5e70035586e6b408b66c64f3b475b17d
Elias Basler <e.e.basler@gmail.com>
2017-12-03 17:32:57 -0500

    Add toggle

--- robot/control.py
+++ robot/control.py
@@ -1,11 +1,30 @@
 class Toggle:
-    def _init_():
-        pass
-    #.on
-    #.off
-    #
+    def _init_(self, function):
+        """Construct an instance of a Toggle object.
 
-class Logic:
-    def _init_():
-        pass
-    #don't need
+        :type  function: function
+        :param function: The function to use as a toggle.
+        """
+        self.function = function
+        self.prevOn = False
+        self.prevOff = True
+
+    @property
+    def on(self):
+        vfunc = self.function()
+        if vfunc and !self.prevOn:
+            return True
+        else:
+            return False
+        self.prevOn = vfunc
+
+    @property
+    def off(self):
+        vfunc = self.function()
+        if !vfunc and self.prevOff:
+            return True
+        else:
+            return False
+        self.prevOff = vfunc
+
+
